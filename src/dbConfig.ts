3030364228edb0e01129fda34161ea7ade635f68
Alexandre Janin <alexandrejanin92@gmail.com>
2019-07-17 19:32:10 +0200

    Added sample database config file

---
+++ src/dbConfig.ts
@@ -0,0 +1,9 @@
+/*
+ * Connection information for the PostgreSQL database.
+ */
+
+export const dbUser = "tierlistserver";
+export const dbHost = "localhost";
+export const dbDatabase = "tierlists";
+export const dbPassword = "";
+export const dbPort = 5432;
